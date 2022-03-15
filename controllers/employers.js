7ddf1d8fd66256e68d434a06b8844833d6400cef
jenni <jenni@DESKTOP-RE2HD7E>
2022-03-14 22:36:13 -0400

    finished CRUD

--- controllers/employers.js
+++ controllers/employers.js
@@ -29,6 +29,55 @@ router.get('/create',(req,res) =>{
     })
 })
 
+/*POST/*/
+router.post('/create',(req,res) => {
+    //use our Mongoose model to create a new employer fro the submitter form body
+    Employer.create(req.body, (err,employer) => {
+        if (err){
+            console.log(err)
+        }
+        else{
+            res.redirect('/employers')
+        }
+    })
 
+})
+
+/*GET/ employers/delete/abc123 => delete employer with ID found in url parameter */
+router.get('/delete/:_id', (req,res) => {
+    Employer.remove({_id: req.params_id}, (err) => {
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.redirect('/employers')
+        }
+    })
+})
+/*GET/ employers/edit/abc123 => show edit from populated with values of selected employer document from url parameter*/ 
+router.get('/edit/:_id', (req, res) => {
+    Employer.findById(req.params._id,(err, employer) =>{
+        if (err){
+            console.log(err)
+        }
+        else{
+            res.render('employers/edit', {
+                title: 'Employer Details',
+                employer: employer
+            })
+        }
+    })
+})
+/*POST / employers/edit/abc123 => update document in mongodb & redirect to index*/
+router.post('/edit/:_id',(req,res) => {
+    Employer.findByIdAndUpdate({ _id: req.params._id }, req.body, null,(err, employer ) => {
+        if(err) {
+            console.log(err)
+        }
+        else{
+            res.redirect('/employers')
+        }
+    })
+})
 // express this file so it is public
 module.exports = router 
---
+++ public/javascripts/scripts.js
@@ -0,0 +1,4 @@
+// re-usable delet co9nfirmation to be called on click of any delete
+function confirmDelete(){
+    return confirm('Are you sure about deleting this?')
+}
